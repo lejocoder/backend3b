41d6dd52d0dc36a987eaf22bd9fbc3dc04f3061a
john le <jcle1@uci.edu>
2019-07-26 20:07:21 -0700

    uibuild

---
+++ build/precache-manifest.c5ddfcdad3f8d153ecfbbad9b230880f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd29153c25f7b8062c545afd13cbffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9528aefb447be53068",
+    "url": "/static/css/main.942e5408.chunk.css"
+  },
+  {
+    "revision": "f642c47fe49ad3c6d22b",
+    "url": "/static/js/2.b54ad857.chunk.js"
+  },
+  {
+    "revision": "2c9528aefb447be53068",
+    "url": "/static/js/main.4a83d891.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
